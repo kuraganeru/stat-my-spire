32d3b7faf742c3de6cf7f18889316499f1aa9adc
andy <n.andyheng@gmail.com>
2023-01-14 16:09:10 -0500

    test returnNeowBonusesArr

--- server/tests/neowBonuses.test.js
+++ server/tests/neowBonuses.test.js
@@ -4,8 +4,7 @@ test("Returns a card received from Neow bonus, if it exists", () => {
     const formattedCardBonus = {
         original_name: 'RANDOM_COLORLESS_2',
         formatted_name: 'Choose a rare colorless card to obtain',
-        has_bonus_card: true,
-        has_bonus_relic: false
+        has_bonus_card: true
     }
     const noCardBonus = {
         has_bonus_card: false
@@ -41,4 +40,39 @@ test("Returns a relic received from Neow bonus, if it exists", () => {
 
     const expected = "Vajra"
     expect(nb.getNeowBonusRelic(formattedCardBonus, relicList)).toEqual(expected)
+})
+
+test("Returns an array of Neow bonus(es) objects with formatted names", () => {
+    const bonus = ["RANDOM_COMMON_RELIC"]
+    const skippedBonus = [ 'RANDOM_COLORLESS', 'TWENTY_PERCENT_HP_BONUS', 'BOSS_RELIC' ]
+    const expectedBonus = [
+        {
+          original_name: 'RANDOM_COMMON_RELIC',
+          formatted_name: 'Obtain a random common relic',
+          has_bonus_card: false,
+          has_bonus_relic: true
+        }
+      ]
+    const expectedSkipped = [
+        {
+          original_name: 'RANDOM_COLORLESS',
+          formatted_name: 'Obtain a random colorless card',
+          has_bonus_card: true,
+          has_bonus_relic: false
+        },
+        {
+          original_name: 'TWENTY_PERCENT_HP_BONUS',
+          formatted_name: 'Gain 20% Max HP (Depends on character)',
+          has_bonus_card: false,
+          has_bonus_relic: false
+        },
+        {
+          original_name: 'BOSS_RELIC',
+          formatted_name: 'Obtain a random boss relic',
+          has_bonus_card: false,
+          has_bonus_relic: true
+        }
+      ]
+    expect(nb.returnNeowBonusesArr(bonus)).toEqual(expectedBonus)
+    expect(nb.returnNeowBonusesArr(skippedBonus)).toEqual(expectedSkipped)
 })
