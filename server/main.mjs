4acee5c37e4bd03659abdedcf49c588fbd49e0fc
andy <n.andyheng@gmail.com>
2023-01-14 17:23:29 -0500

    delete old code from main

--- server/main.mjs
+++ server/main.mjs
@@ -9,7 +9,7 @@ import { readFile } from 'fs/promises';
 import { charactersList } from './game_data/characters.mjs'
 import {returnFormattedDeck} from "./modules/formatDeck.mjs"
 import { returnFormattedNeowBonus } from './modules/neowBonuses.mjs'
-
+import { relicData } from './modules/relics.mjs'
 
 // express
 const app = express()
@@ -218,34 +218,6 @@ const runInfo = (runData) => {
 		character: formatCharacterChosen(character_chosen),
 		neow_bonus: returnFormattedNeowBonus(neowBonusArr, neow_cost, card_choices, neow_bonuses_skipped_log, relic_stats)
 	}
-}
-
-const relicData = (data) => {
-	const {relics_obtained, relic_stats} = data
-
-	/*
-		relics_obtained: [{floor: x, key: relic}]
-		relic_stats: {relic: x, relic2: y} -> [{relic_name: relic, relic_stat: x}]
-	*/
-	const {obtain_stats, counters, ...filterRelicStats} = relic_stats
-	const relicReducer = (relicObj, key) => {
-		return Object.entries(relicObj).reduce((total, current) => [...total, {relic_name: current[0], [key]: current[1]}],[])
-	}
-	const relicStats = relicReducer(filterRelicStats, "relic_stats")
-	const relicFloors = relicReducer(obtain_stats[0], "floor_found")
-
-	// obtain_stats - some are integers, some are arrays
-	// might change all ints to arrs
-	  // map
-	const allRelicData = relicFloors.map(val => {
-		const foundRelicWithStats = relicStats.find(x => x.relic_name === val.relic_name)
-		return {
-			...val,
-			floor_found: val.floor_found,
-			relic_stats: foundRelicWithStats ? foundRelicWithStats.relic_stats : null
-		}
-	})
-	return allRelicData
 }
 
 const formatRunData = rawRunData => {
