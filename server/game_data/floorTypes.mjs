d8f81cf34c027d065f312901b4014c31f7fc95b5
andy <n.andyheng@gmail.com>
2023-01-16 20:15:22 -0500

    extract floorTypes into game_data file

---
+++ server/game_data/floorTypes.mjs
@@ -0,0 +1,16 @@
+const floorTypes = [
+    { orig_name: "M", new_name: "Monster" },
+    { orig_name: "?", new_name: "Question Mark" },
+    { orig_name: "$", new_name: "Shop" },
+    { orig_name: "E", new_name: "Elite" },
+    { orig_name: "BOSS", new_name: "Boss" },
+    { orig_name: "R", new_name: "Rest Site" },
+    { orig_name: "T", new_name: "Treasure" },
+    { orig_name: "AB", new_name: "After Boss" },
+    { orig_name: "QEV", new_name: "Event" },
+    { orig_name: "QM", new_name: "Unknown / Monster" },
+    { orig_name: "Q$", new_name: "Unknown / Shop" },
+    { orig_name: "QT", new_name: "Unknown / Treasure" }
+]
+
+export { floorTypes }
