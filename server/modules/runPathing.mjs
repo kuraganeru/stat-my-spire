6ac3493246a090328cf1565ee756e63ee3737bfd
andy <n.andyheng@gmail.com>
2023-01-16 20:50:26 -0500

    initialize formatPathTaken function and testing

--- server/modules/runPathing.mjs
+++ server/modules/runPathing.mjs
@@ -9,4 +9,18 @@ const formatPathPerFloor = arr => {
     })
 }
 
-export { formatPathPerFloor }
+/*
+    Decrements through an array, and splices in an "AB" value after every instance of "BOSS"
+    testing - path[i+1] in conditional not required?
+*/
+const formatPathTaken = arr => {
+    let path = [...arr]
+    for (let i = path.length - 1; i >= 0; i--) {
+        if (path[i] === "BOSS" && path[i+1] != "BOSS") {
+            path.splice(i + 1, 0, "AB")
+        }
+    }
+    return path
+}
+
+export { formatPathPerFloor , formatPathTaken}
--- server/tests/runPathing.test.js
+++ server/tests/runPathing.test.js
@@ -1,4 +1,4 @@
-import { formatPathPerFloor } from "../modules/runPathing.mjs";
+import { formatPathPerFloor, formatPathTaken } from "../modules/runPathing.mjs";
 
 test("Replaces null value with 'AB' or returns original value", () => {
     const with_null = ['M', 'E', null, 'B', null]
@@ -7,4 +7,12 @@ test("Replaces null value with 'AB' or returns original value", () => {
     const without_expected = ['M', 'E', 'T']
     expect(formatPathPerFloor(with_null)).toEqual(with_expected)
     expect(formatPathPerFloor(without_null)).toEqual(without_expected)
+})
+
+test("Splices in 'AB' value after 'BOSS' values", () => {
+    const without_AR = [ "M", "BOSS", "M", "BOSS", "M", "BOSS", "R", "BOSS" ]
+    const with_expected = [ "M", "BOSS", "AB", "M", "BOSS", "AB", "M", "BOSS", "AB", "R", "BOSS", "AB" ]
+    
+    expect(formatPathTaken(real)).toEqual(real_formatted)
+    expect(formatPathTaken(without_AR)).toEqual(with_expected)
 })
