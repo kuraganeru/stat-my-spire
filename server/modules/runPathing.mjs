83eb555b7e13d2bc8f360c65793cc50a2560f9c1
andy <n.andyheng@gmail.com>
2023-01-16 20:24:50 -0500

    initialize formatPathPerFloor function

--- server/modules/runPathing.mjs
+++ server/modules/runPathing.mjs
@@ -1,1 +1,10 @@
 import { floorTypes } from "../game_data/floorTypes.mjs"
+
+const formatPathPerFloor = arr => {
+    return arr.map(val => {
+        if (val === null) {
+            return "AB"
+        }
+        return val
+    })
+}
