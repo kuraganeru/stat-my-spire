bd0b334584c09d1c54ea81e03af57af84385d847
andy <n.andyheng@gmail.com>
2023-01-16 20:31:13 -0500

    initialize testing for formathPathPerFloor

--- server/tests/runPathing.test.js
+++ server/tests/runPathing.test.js
@@ -1,2 +1,10 @@
 import { formatPathPerFloor } from "../modules/runPathing.mjs";
 
+test("Replaces null value with 'AB' or returns original value", () => {
+    const with_null = ['M', 'E', null, 'B', null]
+    const without_null = ['M', 'E', 'T']
+    const with_expected = ['M', 'E', 'AB', 'B', 'AB']
+    const without_expected = ['M', 'E', 'T']
+    expect(formatPathPerFloor(with_null)).toEqual(with_expected)
+    expect(formatPathPerFloor(without_null)).toEqual(without_expected)
+})
