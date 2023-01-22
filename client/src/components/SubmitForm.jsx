048b2afc426a3be9e93ebeed3e6c311af696ec43
andy <n.andyheng@gmail.com>
2023-01-22 16:45:48 -0500

    split submitForm into separate component

---
+++ client/src/components/SubmitForm.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const SubmitForm = (props) => {
+
+    const handleSetRawRun = ev => {
+        props.setRawRun(ev.target.files[0])
+    }
+
+    const createFormData = uploadedValue => {
+        let formDataToUpload = new FormData()
+        formDataToUpload.append('runData', uploadedValue)
+        return formDataToUpload
+    }
+
+    const uploadFormData = async formData => {
+        const uploadUrl = 'http://localhost:5000/upload_files'
+        const uploadParams = {
+            method: "POST",
+            body: formData
+        }
+
+        const response = await fetch(uploadUrl, uploadParams)
+        const responseJSON = await response.json()
+        return responseJSON
+    }
+
+    const handleSubmit = async ev => {
+        ev.preventDefault();
+        try {
+            const createdFormData = createFormData(props.rawRun)
+            const uploadResponse = await uploadFormData(createdFormData)
+            props.setFormattedRun(uploadResponse)
+        } catch (error) {
+            throw new Error(error)
+        } finally {
+            console.log("Upload complete")
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="file" name="files" id="files" accept=".run" onChange={handleSetRawRun}/>
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
+
+export default SubmitForm
