c2842ac37401168cd8eba82a654dcca34782ff10
andy <n.andyheng@gmail.com>
2023-01-22 16:46:58 -0500

    import SubmitForm component and remove old code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,33 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import { useEffect } from 'react'
 
+// Components
+import SubmitForm from './components/SubmitForm'
+
 function App() {
   const [rawRun, setRawRun] = useState('')
   const [formattedRun, setFormattedRun] = useState({})
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    
-    // create formdata
-    let formData = new FormData();
-    formData.append('runData', rawRun)
-
-    // upload formdata to backend
-    const uploadRunData = async () => {
-      const response = await fetch('http://localhost:5000/upload_files', {
-        method: "POST",
-        body: formData
-      })
-      const responseJSON = await response.json();
-      setFormattedRun(responseJSON)
-    }
-    uploadRunData()
-    // console.log(formattedRun) doesn't work here! see useeffect below
-  }
-
-  const handleSubmitRun = (ev) => {
-    setRawRun(ev.target.files[0])
-  }
 
   /*
       good enough explanation: setState / setFormattedRun in the handleSubmit function above is async - it lets the console.log(formattedRun) execute while it sets the value. which is also why the empty console.log on line 27 appears before, if we were to put a console.log at l25 
@@ -43,10 +22,7 @@ function App() {
   return (
     <div className="App">
       <h1>Stat My Spire</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="file" name="files" id="files" accept=".run" onChange={handleSubmitRun}  />
-        <button type="submit">Submit</button>
-      </form>
+      <SubmitForm setFormattedRun={setFormattedRun} setRawRun={setRawRun} rawRun={rawRun} />
       <h2>{formattedRun.character}</h2>
     </div>
   )
