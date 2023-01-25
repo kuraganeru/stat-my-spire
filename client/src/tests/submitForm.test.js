0133e4283f549bc28e692ceaa52db62edbf83cf6
andy <n.andyheng@gmail.com>
2023-01-25 18:58:37 -0500

    import react testing libraries

--- client/src/tests/submitForm.test.js
+++ client/src/tests/submitForm.test.js
@@ -1,3 +1,12 @@
+// import React so you can use JSX (React.createElement) in your test
+import React from 'react'
+
+/**
+ * render: lets us render the component as React would
+ * screen: a utility for finding elements the same way the user does
+ */
+import {render, screen} from '@testing-library/react'
+
 import { uploadFormData } from "../components/SubmitForm";
 
 beforeEach(() => {
