5c0858cb6c2a17e466b40e40a56402041fdd9281
andy <n.andyheng@gmail.com>
2023-01-25 20:35:35 -0500

    update tests for additional headers

--- client/src/tests/runSummary.test.js
+++ client/src/tests/runSummary.test.js
@@ -3,7 +3,8 @@ import { render, screen } from '@testing-library/react'
 import RunSummary from '../components/RunSummary'
 
 test("Has the character value from the uploaded file", () => {
-    const formattedRun = { character: "The Silent" }
+    const formattedRun = { character: "The Silent", ascension_level: 5 }
     render(<RunSummary formattedRun={formattedRun} />)
-    expect(screen.getByRole('heading')).toHaveTextContent('The Silent')
+    expect(screen.getByRole('heading', {level: 2})).toHaveTextContent('The Silent')
+    expect(screen.getByRole('heading', {level: 3})).toHaveTextContent('5')
 })
