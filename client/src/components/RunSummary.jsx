a65163b0bd7c8e8befd7375f327f2116d992fc00
kuroganeru <n.andyheng@gmail.com>
2023-01-28 12:15:55 -0500

    import react-bootstrap container

--- client/src/components/RunSummary.jsx
+++ client/src/components/RunSummary.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import RunHeader from './RunHeader'
 
 const RunSummary = props => {
     return (
-        <div>
-            <RunHeader formattedRun={props.formattedRun} />
-        </div>
+        <Container id="run-summary-header">
+            <Row>
+                <Col>
+                    <RunHeader formattedRun={props.formattedRun} />
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
