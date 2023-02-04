6d431f3dc4ab1ec8de801d3119253da898006acf
andy <n.andyheng@gmail.com>
2023-02-04 13:11:13 -0500

    init function for formatting win/loss message

--- client/src/components/RunHeader.jsx
+++ client/src/components/RunHeader.jsx
@@ -2,6 +2,15 @@ import React, { Fragment } from 'react'
 
 const RunHeader = props => {
 
+    const formatWinLossResult = (didWin, floorList) => {
+        if (didWin) {
+            return "Victory!"
+        } else {
+            const endingFloor = floorList[floorList.length - 1]
+            return `Slain on Floor ${endingFloor.floor} by ${endingFloor.enemies}`
+        }
+    }
+
     const renderFormattedRun = (formattedRun) => {
         if (!formattedRun || Object.keys(formattedRun).length <= 0) {
             return null
@@ -9,8 +18,8 @@ const RunHeader = props => {
         return (
             <>
                 <h2>{formattedRun.character}</h2>
-                <h3>Ascension Level {formattedRun.ascension_level}</h3>
-                <h3>{formattedRun.victory ? "Victory!" : "Defeat..."}</h3>
+                <h3>{formatWinLossResult(formattedRun.victory, formattedRun.run_nodes)}</h3>
+                <h4>Ascension Level {formattedRun.ascension_level}</h4>
             </>
         )
     }
